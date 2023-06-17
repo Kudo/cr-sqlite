011cdd815d7d41d8a81baa95b28a6fd6fe7c66c8
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-17 14:25:55 -0400

    start the db cache re-write

---
+++ js/packages/crsqlite-wasm/src/db2/DB2.ts
@@ -0,0 +1,5 @@
+/**
+ * A re-write of the DB wrapper from the lessons learned after fixing up Riffle's DB class.
+ *
+ * Our case, however, is more complicated given the interface to the db is async.
+ */
